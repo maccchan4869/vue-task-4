4a301ca78c854cb5fe84b220232fa820b8a14c5f
maccchan4869 <ymatsio554@gmail.com>
2021-01-25 20:50:22 +0900

    要件2 ログイン機能実装

--- src/store/index.js
+++ src/store/index.js
@@ -13,26 +13,40 @@ export default createStore({
   actions: {
     // アカウントを作成
     async registerAccount({ dispatch }, {displayName, email, password}) {
-      await firebase.auth().createUserWithEmailAndPassword(email, password).catch(error => {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(email, password);
+        dispatch('updateUserName', displayName);
+      } catch (error) {
         console.log(error.message);
-      });
-      dispatch('updateUserName', displayName);
+      }
     },
 
     // ユーザー名を更新
     async updateUserName({ commit }, displayName) {
-      commit('commitLoginUserName', displayName);
-      const user = firebase.auth().currentUser;
-      if(user === null) return;
-      await user.updateProfile({
-        displayName: displayName
-      }).catch(error => {
+      try {
+        commit('commitLoginUserName', displayName);
+        const user = firebase.auth().currentUser;
+        if(user === null) return;
+        await user.updateProfile({displayName: displayName});
+        alert(`ユーザー名を「${displayName}」に更新しました`);
+      } catch (error) {
         console.log(error.message);
-      });
+      }
+    },
 
-      // デバック用（後にログイン処理に変更）
-      alert(`ユーザー名を「${displayName}」に更新しました`);
-    }
+    // ログイン
+    async login({ commit }, {email, password}) {
+      try {
+        await firebase.auth().signInWithEmailAndPassword(email, password);
+        const user = firebase.auth().currentUser;
+        if(user === null) return;
+        commit('commitLoginUserName', user.displayName);
+        // デバック用（後にページ遷移処理に変更）
+        alert(`ユーザー名「${user.displayName}」でログインしました`);
+      } catch (error) {
+        console.log(error.message);
+      }
+    },
   },
   modules: {
   }
