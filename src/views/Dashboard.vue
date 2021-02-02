db2b86f22c493279acfbc91436c666eb507fcedc
maccchan4869 <ymatsio554@gmail.com>
2021-02-02 23:01:57 +0900

    要件4 ログアウト機能実装

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,11 +3,13 @@
     <div class="loginUserInfo">
       <div class="divLoginUserName">{{ loginUser.userName }}さんようこそ！！</div>
       <div class="divWallet">残高：{{ loginUser.wallet }}</div>
+      <input type="button" class="btn btn-outline-primary btnLogout" value="ログアウト" @click="logout">
     </div>
   </div>
 </template>
 
 <script>
+import firebase from 'firebase'
 export default {
   name: 'dashboard',
   data () {
@@ -23,6 +25,17 @@ export default {
     this.loginUser = this.$store.getters.getLoginUser;
   },
   methods: {
+    // ログアウト
+    async logout() {
+      try {
+        await firebase.auth().signOut();
+        this.$store.commit('commitLoginUser', {uid: '', userName: '', wallet: 0});
+        console.log('ログアウトしました');
+        this.$router.push('/');
+      } catch (error) {
+        console.error(error.message);
+      }
+    },
   }
 }
 </script>
@@ -30,7 +43,11 @@ export default {
 <style>
 .divLoginUserName, .divWallet {
   display: inline-block;
-  width: 50%;
+  width: 40%;
   font-size: 20px;
+}
+
+.btnLogout {
+  display: inline-block;
 }
 </style>
